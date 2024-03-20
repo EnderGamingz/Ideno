d04b340dd72dda83c99e868560f80e958019133d
Philipp Hergenhahn <philipp.hergenhahn@setilic.com>
2024-03-20 13:46:19 +0100

    Remove testing setup and associated dependencies
    Deleted test configurations and removed Jest along with related testing libraries and scripts. This includes updates to `package.json` and `pnpm-lock.yaml` to reflect the removal of testing dependencies.

--- src/app/page.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import Page from './page';
-
-describe('Start page', () => {
-  it('Page renders', async () => {
-    render(await Page());
-    expect(screen.getByRole('heading').textContent).toBe('Not signed in');
-  });
-});
